5eb91b8abd5c2420bcc7f1f5f74f6eacdb1f0174
Ubuntu <ubuntu@ip-172-31-19-61.us-east-2.compute.internal>
2017-11-16 06:59:49 +0000

    res

--- model/infer.py
+++ model/infer.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 from tensorflow.contrib.layers.python.layers import batch_norm
 from DataLoader import *
 
-# Dataset Parameters
 batch_size = 1
 load_size = 256
 fine_size = 224
@@ -18,24 +17,23 @@ opt_data_test = {
     'data_mean': data_mean,
     'randomize': False,
     'perm': False
-    }
+}
 
 loader_test = DataLoaderDisk(**opt_data_test)
-
-# tf Graph input
-saver = tf.train.import_meta_graph('./model_out/res-1800.meta')
-
+saver = tf.train.import_meta_graph('./model_out/model-16000.meta')
 sess = tf.Session()
-# saver.restore(sess, tf.train.latest_checkpoint('./model_out/'))
-saver.restore(sess, './model_out/res-2000')
+saver.restore(sess,'./model_out/model-16000')
+# saver.restore(sess, tf.train.latest_checkpoint('./model_out/res-6000.meta'))
+# saver.restore(sess, './model_out/res-6000')
 
 graph = tf.get_default_graph()
 x = graph.get_tensor_by_name("x:0")
 y = graph.get_tensor_by_name("y:0")
-# keep_dropout = graph.get_tensor_by_name("keep_dropout:0")
-# train_phase = graph.get_tensor_by_name("train_phase:0")
+keep_dropout = graph.get_tensor_by_name("keep_dropout:0")
+train_phase = graph.get_tensor_by_name("train_phase:0")
 logits = graph.get_tensor_by_name("logits:0")
-values,indices = tf.nn.top_k(logits,k=5)
+softmax = tf.nn.softmax(logits)
+values,indices = tf.nn.top_k(softmax,k=5)
 print('Inference on the whole test set...')
 num_batch = loader_test.size()//batch_size
 with open("../data/test.txt",'r') as f:
@@ -45,8 +43,8 @@ with open("../data/test.txt",'r') as f:
         in_lines = f.readlines()
         for i in range(num_batch):
             images_batch, labels_batch = loader_test.next_batch(batch_size)
-            # p = sess.run(indices, feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False})
-            p = sess.run(indices, feed_dict={x: images_batch, y: labels_batch})
+            p = sess.run(indices, feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False})
+            # p = sess.run(softmax, feed_dict={x: images_batch, y: labels_batch, train_phase: False})
             in_line = in_lines[i].split(" ")
             file_name = in_line[0]
             out_line = file_name
