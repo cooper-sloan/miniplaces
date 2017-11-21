da31476d9f92ce4913a070b2d86543cf3bdc5bab
logoman04 <lcmartin@mit.edu>
2017-11-20 20:46:31 -0500

    More graphs

--- model/graphs/graph.py
+++ model/graphs/graph.py
@@ -14,11 +14,13 @@ data = {}
 with open(filename, 'r') as file:
 	data = pickle.load(file)
 
-
+batch_size = 250
+step_display = 50
 n = np.arange(len(data['top1_v']))
+n = n*step_display
 
-plt.plot(n, data['top1_t'], label='t1')
-plt.plot(n, data['top5_t'], label='t5')
+#plt.plot(n, data['top1_t'], label='t1')
+#plt.plot(n, data['top5_t'], label='t5')
 plt.plot(n, data['top1_v'], label='v1')
 plt.plot(n, data['top5_v'], label='v5')
 plt.legend()
